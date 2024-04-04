ba4eed7aae129ba7b8746b21232e11999fbeebb6
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-04-04 18:31:53 +0200

    Repair build

---
+++ Nivaes.Typst.UnitTest/UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Nivaes.Typst.UnitTest.UnitTests
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
