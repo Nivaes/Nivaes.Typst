1c53c4f714be7e0fdd86a18b53a83564e8fcc6f3
Andrés Giménez <andres.gimenez@outlook.com>
2024-04-06 17:48:43 +0200

    Run in linux

--- Nivaes.Typst/Sources/RustInterop.cs
+++ Nivaes.Typst/Sources/RustInterop.cs
@@ -10,7 +10,17 @@ namespace Nivaes.Typst
 {
     public class RustInterop
     {
-        [DllImport("./Native/typst_ffi", EntryPoint = "add_numbers")]
+#if AOT
+        const string NativeLibrary = "*";
+#elif WINDOWS
+        private const string NativeLibrary = "Native/typst_ffi.dll";
+#elif LINUX
+        const string NativeLibrary = "Native/libtypst_ffi.so";
+#elif MACOS
+        private const string NativeLibrary = "Native/libtypst_ffi.dylib";
+#endif
+
+        [DllImport(NativeLibrary, EntryPoint = "add_numbers", ExactSpelling = true, CallingConvention = CallingConvention.Cdecl)]
         private static extern Int32 add_numbers(Int32 number1, Int32 number2);
 
         public static int AddNumbers(int number1, int number2)
